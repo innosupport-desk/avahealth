6513b70f89cd2ba9cf620ea45ab82e43ee600be8
oadekunlee <114141496+ThankG12@users.noreply.github.com>
2025-06-24 09:26:10 +0100

    Remove unnecessary React import from Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Heart, Menu, X, Mail } from 'lucide-react';
 
